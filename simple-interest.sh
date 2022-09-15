8255454213a18dc7c65fd09284532cd6f2534f0f
Mazen <mazen.is@gmail.com>
2022-09-16 00:31:43 +0200

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -6,12 +6,12 @@
 
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # mazen.is@gmail.com
 
    # Input:
    # p, principal amount
    # t, time period in years
-   # r, annual rate of interest
+   # r, annual rate of interest  
 
    # Output:
    # simple interest = p*t*r
